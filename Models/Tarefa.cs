fe74fa3097aa204c9fece27c6790d7bc430af392
Rui Gabriel <ruigoliveira1@gmail.com>
2025-08-16 17:44:40 -0300

    Desafio finalizado

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -8,8 +8,8 @@ namespace TrilhaApiDesafio.Models
     public class Tarefa
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Descricao { get; set; }
+        public string Titulo { get; set; } = default !;
+        public string Descricao { get; set; } = default!;
         public DateTime Data { get; set; }
         public EnumStatusTarefa Status { get; set; }
     }
